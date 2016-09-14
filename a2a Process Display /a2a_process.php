0a595a59f858f9021dd1773c5aa5f874d5610969
tbumgarner <travis_bumgarner@abtassoc.com>
2016-09-14 16:41:33 -0400

    Changed echo to return

--- a2a Process Display /a2a_process.php
+++ a2a Process Display /a2a_process.php
@@ -1,3 +1,4 @@
+<?php
 function a2a_process($user_class_toggles){
 	#Function generates The 5 images and text for "Plan, Learn, Analyze, Share, Act"
 
@@ -24,14 +25,16 @@ function a2a_process($user_class_toggles){
 	}
 	
 	#generate a div containing a wrapper and each of the five components
-	echo '<div class = "process_wrapper">';
-	echo '<div class ="' . ($merged_class_toggles['process_plan']? "process_plan" : "process_not_active") . '"><p>Plan</p></div>';
-	echo '<div class ="' . ($merged_class_toggles['process_learn'] ? "process_learn" : "process_not_active") . '"><p>Learn</p></div>';
-	echo '<div class ="' . ($merged_class_toggles['process_analyze'] ? "process_analyze" : "process_not_active") . '"><p>Analyze</p></div>';
-	echo '<div class ="' . ($merged_class_toggles['process_share'] ? "process_share" : "process_not_active") . '"><p>Share</p></div>';
-	echo '<div class ="' . ($merged_class_toggles['process_act'] ? "process_act" : "process_not_active") . '"><p>Act</p></div>';
-	echo'</div><!-- /process_wrapper -->';
+	$content = '<div class = "process_wrapper">';
+	$content .= "<div class =\"" . ($merged_class_toggles['process_plan']    ? "process_plan"    : "process_not_active") . '"><p>Plan</p></div>';
+	$content .= "<div class =\"" . ($merged_class_toggles['process_learn']   ? "process_learn"   : "process_not_active") . '"><p>Learn</p></div>';
+	$content .= "<div class =\"" . ($merged_class_toggles['process_analyze'] ? "process_analyze" : "process_not_active") . '"><p>Analyze</p></div>';
+	$content .= "<div class =\"" . ($merged_class_toggles['process_share']   ? "process_share"   : "process_not_active") . '"><p>Share</p></div>';
+	$content .= "<div class =\"" . ($merged_class_toggles['process_act']     ? "process_act"     : "process_not_active") . '"><p>Act</p></div>';
+	$content .= '</div><!-- /process_wrapper -->';
+return $content;
 }
 
 #Convert php funtion to Wordpress shortcode
 add_shortcode( 'a2a_process', "a2a_process");
+?>
