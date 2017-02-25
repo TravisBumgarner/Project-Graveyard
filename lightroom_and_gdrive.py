cc6273ee8add6cb1790ff27e485c6599f2fe4bb7
Travis Bumgarner <travis.bumgarner@gmail.com>
2017-02-24 21:44:40 -0500

    Cleaned up code

--- lightroom_and_gdrive.py
+++ lightroom_and_gdrive.py
@@ -1,18 +1,20 @@
-def gdrive_running():
-	import os
-	import psutil
-	google_drive_found = False
-	for pid in psutil.pids():
-		p = psutil.Process(pid)
-		if p.name() == "googledrivesync.exe":
-			google_drive_found = True
-	if(google_drive_found == True):
-		ign = input("Close Google Drive and then press enter. (Type 'ign' if you've already closed it.")
-		if(ign == "ign"):
-			os.startfile("C:\Program Files\Adobe\Adobe Lightroom\lightroom.exe")
-		else:
-			gdrive_running()
-	else:
-		os.startfile("C:\Program Files\Adobe\Adobe Lightroom\lightroom.exe")
+import os
+import psutil
 
-gdrive_running()
+def gdrive_running():
+    google_drive_found = False
+    for pid in psutil.pids():
+        p = psutil.Process(pid)
+        if p.name() == "googledrivesync.exe":
+            google_drive_found = True
+    if(google_drive_found):
+        ign = input("Close Google Drive and then press enter. (Type 'ign' if you've already closed it.")
+        if(ign == "ign"):
+            os.startfile("C:\Program Files\Adobe\Adobe Lightroom\lightroom.exe")
+        else:
+            gdrive_running()
+    else:
+        os.startfile("C:\Program Files\Adobe\Adobe Lightroom\lightroom.exe")
+
+if __name__ == "__main__":
+    gdrive_running()
