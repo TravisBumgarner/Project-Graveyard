b60012f3e9b236818646511d8cda4c87800c2df7
Travis Bumgarner <travis.bumgarner@gmail.com>
2017-10-01 14:41:00 -0400

    Refactored for multiple APIs

--- src/views/app/index.js
+++ src/views/app/index.js
@@ -17,10 +17,10 @@ class App extends Component {
     };
   }
 
-  getData = () =>{
+  getInstagramData = () =>{
     const surroundingCords = getSurroundingCoords(this.state.textInputLat, this.state.textInputLong, this.state.textInputDistance);
     console.log(surroundingCords);
-    axios.get('http://127.0.0.1:8000/api/get_images')
+    axios.get('http://127.0.0.1:8000/instagram_api/get_images')
       .then(response => {
         let images = response.data.data;
         console.log(images);
@@ -37,14 +37,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <a href="https://api.instagram.com/oauth/authorize/?client_id=b34a768ac36a4f5c877eb9bdd8bae1be&redirect_uri=http://127.0.0.1:8000/api/hello_response&response_type=code&scope=public_content">Enable IG</a>
+        <a href="https://api.instagram.com/oauth/authorize/?client_id=b34a768ac36a4f5c877eb9bdd8bae1be&redirect_uri=http://127.0.0.1:8000/instagram_api/hello_response&response_type=code&scope=public_content">Enable IG</a>
+        <a href="">Enable Flickr</a>
 
 
         <input type="text" name = "textInputLat" value={this.props.textInputLat} onChange={this.handleInputChange}/>
         <input type="text" name = "textInputLong" value={this.props.textInputLong} onChange={this.handleInputChange} />
         <input type="text" name = "textInputDistance" value={this.props.textInputLat} onChange={this.handleInputChange}/>
 
-        <button onClick={this.getData}>Get Images</button>
+        <button onClick={this.getInstagramData}>Get Images</button>
         <div id="tile-wrapper-all">
           <div id ="tile-wrapper-row">
             <Tile numVal={1}>  </Tile>
