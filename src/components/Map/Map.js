06c8259333ae07484e8f7f080a0c9d98c68bdd92
Travis Bumgarner <travis.bumgarner@gmail.com>
2017-12-09 21:19:36 -0500

    wooo looking good. Map works with marker history of where you've been

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -5,6 +5,16 @@ import { connect } from 'react-redux';
 
 import { CENTER_DIRECTION } from '../../utilities/constants'
 
+const icon = (fillOpacity) => {
+  return {
+    path: "M-20,0a20,20 0 1,0 40,0a20,20 0 1,0 -40,0",
+    fillColor: 'rgb(64, 224, 208)',
+    fillOpacity,
+    strokeWeight: 0,
+    scale: 0.3
+  }
+}
+
 const MyMapComponent = compose(
   withProps({
     googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyBGM37DlAcV8oLUNewkPs3e8XSBDQVXOQM&v=3.exp&libraries=geometry,drawing,places",
@@ -19,7 +29,8 @@ const MyMapComponent = compose(
     defaultZoom={8}
     defaultCenter={{ lat: props.lat, lng: props.lon }}
   >
-    {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} onClick={props.onMarkerClick} />}
+    <Marker position={{ lat: props.lat, lng: props.lon }} />
+    { props.markers }
   </GoogleMap>
 )
 
@@ -43,22 +54,42 @@ class Map extends Component {
     this.delayedShowMarker()
   }
 
+  createmarkers = () => {
+    const { history } = this.props;
+    const markers = history.map((item, idx) => {
+      const fillOpacity = ( idx + 1) / history.length;
+      return (
+        <Marker
+          position={{ lat: item.lat, lng: item.lon }}
+          key={ `lat:${item.lat},lon:${item.lon}` }
+          icon={icon(fillOpacity)}
+        />
+      )
+    });
+    return markers;
+  };
 
   render() {
     const { centerTile: {lat, lon}} = this.props;
 
+    const markers = this.createmarkers();
+    console.log(lat, lon);
     return (
       <MyMapComponent
         isMarkerShown={this.state.isMarkerShown}
         onMarkerClick={this.handleMarkerClick}
         lat={lat}
         lon={lon}
-      />
+        markers={markers}
+      >
+
+      </MyMapComponent>
     )
   }
 }
 
 export default connect(state => ({
   centerTile: state.tile.allTiles[CENTER_DIRECTION],
+  history: state.tile.history,
 }), {
 })(Map);
