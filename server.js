e5f74da17fdb080c5304acc9e790a7942ba54a43
Travis Bumgarner <travis.bumgarner@gmail.com>
2017-12-24 19:27:00 +0000

    Added index.ejs

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!sddwti'))
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
 
 app.listen(20603, () => console.log('Example app listening on port 3000!'))
