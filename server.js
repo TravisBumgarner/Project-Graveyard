0db10130dee71fe53282509c9802b64f057a6052
Travis Bumgarner <travis.bumgarner@gmail.com>
2017-12-24 19:47:39 +0000

    WOOOO console.log shows up on index

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express()
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
+app.use(express.static('public'));
 app.get('/', function(req, res) {
     res.render('index');
 });
