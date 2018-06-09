d4be9168a2a77a0c387f85a82b99264456547c59
Travis Bumgarner <travis.bumgarner@gmail.com>
2018-06-09 13:22:38 -0400

    Added code for dev and prod

--- webpack.common.js
+++ webpack.common.js
@@ -1,16 +1,9 @@
 const path = require('path');
-const webpack = require('webpack');
+var webpack = require('webpack');
 
 module.exports = {
   entry: {
     app: './src/index.js'
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    publicPath: '/',
-    contentBase: "./dist",
-    port: 3000,
-    historyApiFallback: true,
   },
   module: {
     rules: [
@@ -23,18 +16,13 @@ module.exports = {
         }
       },
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'eslint-loader'
-      },
-      {
         test: /\.(jpg|png|gif|svg|pdf|ico)$/, // TODO replace this with cloudfront or whatever
         loader: 'file-loader'
       }
     ]
   },
-//   output: {
-//     filename: '[name].bundle.js',
-//     path: path.resolve(__dirname, 'dist')
-//   }
-// };
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
---
+++ webpack.dev.js
@@ -0,0 +1,21 @@
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  devtool: 'inline-source-map',
+  devServer: {
+    publicPath: '/',
+    contentBase: "./dist",
+    port: 3000,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader'
+      }
+    ]
+  },
+});
