21606da858c28d9d6cdc1c6c6dfd7bce606088c4
Travis Bumgarner <travis-bumgarner@pluralsight.com>
2018-07-20 14:15:02 -0400

    Some refactoring, added babel support

--- api/server.js
+++ api/server.js
@@ -1,11 +1,7 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-var elasticsearch = require('elasticsearch')
+import express from 'express'
+import bodyParser from 'body-parser'
+import elasticClient from './elasticClient'
 
-var client = new elasticsearch.Client({
-  host: 'localhost:9200',
-  log: 'trace'
-})
 const app = express()
 
 app.use((req, res, next) => {
@@ -22,25 +18,25 @@ app.use(bodyParser.json({type:'application/vnd.api+json'}))
 const port = process.env.PORT || 8000
 
 app.get('/ok', (req, res, next) => {
-  client.cluster.health({},function(err,resp,status) {  
+  elasticClient.cluster.health({},function(err,resp,status) {  
     res.json({resp})
   })
 })
 
 app.get('/indices', (req, resNode, next) => {
-  client.cat.indices({format: 'json'}, function(err,resElastic,status) {  
+  elasticClient.cat.indices({format: 'json'}, function(err,resElastic,status) {  
     resNode.send(resElastic)
   })
 })
 
-app.get('/indices/delete', (req, resNode, next) => {
-  client.indices.delete({index: req.query.index}, function(err,resElastic,status) {  
-    resNode.send(resElastic)
-  })
-})
+// app.get('/indices/delete', (req, resNode, next) => {
+//   elasticClient.indices.delete({index: req.query.index}, function(err,resElastic,status) {  
+//     resNode.send(resElastic)
+//   })
+// })
 
 app.get('/indices/create', (req, resNode, next) => {
-  client.indices.create({index: req.query.index}, function(err,resElastic,status) {  
+  elasticClient.indices.create({index: req.query.index}, function(err,resElastic,status) {  
     resNode.send(resElastic)
   })
 })
