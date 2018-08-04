61123e578adce5e7bd62774891e8f21cf26eaa60
Travis Bumgarner <travis.bumgarner@gmail.com>
2018-08-04 15:08:24 -0400

    Added buttons to create form

--- src/views/CreateSnippet/CreateSnippet.js
+++ src/views/CreateSnippet/CreateSnippet.js
@@ -2,20 +2,42 @@ import React, { Component } from 'react'
 // import PropTypes from 'prop-types'
 
 import { CreateSnippetWrapper } from './CreateSnippet.styles'
-import { Textbox } from '../../components'
+import { Textbox, Button } from '../../components'
 
 export default class CreateSnippet extends Component {
     constructor(props){
         super(props)
         this.state = {
-            'who': 'whooo',
-            'what': 'whattt',
-            'where': 'wherreee',
+            'who': '',
+            'what': '',
+            'where': '',
         }
     }
 
     handleInputChange = (name, value) => {
         this.setState({[name]: value})
+    }
+
+    clearForm = () => {
+        this.setState({
+            who: '',
+            what: '',
+            where: '',
+        })
+    }
+
+    handleSubmit = () => {
+        const {
+            who,
+            what,
+            where
+        } = this.state
+        console.log(`${who}\n${what}\n${where}`)
+        this.clearForm()
+    }
+
+    handleCancel = () => {
+        alert('canceled')
     }
     
     render() {
@@ -45,6 +67,8 @@ export default class CreateSnippet extends Component {
                     name="where"
                     value={where}
                 />
+                <Button onClick={this.handleSubmit}>Submit</Button>
+                <Button onClick={this.handleCancel}>Cancel</Button> 
             </CreateSnippetWrapper>
         )
     }
