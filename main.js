45298d7321711bdd055c9004fe9a05a3f1a9a905
Travis Bumgarner <travis.bumgarner@gmail.com>
2019-01-04 18:59:07 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,25 @@
-let speedIdx = 2
-const speedMultiplyers = [0.5, 0.75, 1, 2, 4, 8]
+(()=> {
+    let speedIdx = 3 // For me, defaults to 2x speed.
+    const speedMultiplyers = [0.5, 0.75, 1, 2, 4, 6, 8]
 
-// Visit https://keycode.info/ to remap the keys below. 
-const INCREASE_SPEED = 93 // this is the [ char
-const DECREASE_SPEED = 91 // this is the ] char
+    // Visit https://keycode.info/ to remap the keys below.
+    const DECREASE_SPEED = 219 // this is the ] char
+    const INCREASE_SPEED = 221 // this is the [ char
 
-document.addEventListener('keypress', event => {
-    if(event.keyCode === INCREASE_SPEED && speedIdx < speedMultiplyers.length){
-        speedIdx += 1
-    } else if (event.keyCode === DECREASE_SPEED && speedIdx > 0) {
-        speedIdx -= 1
+    document.addEventListener('keydown', event => {
+        if(event.keyCode === INCREASE_SPEED && speedIdx < speedMultiplyers.length){
+            speedIdx += 1
+        } else if (event.keyCode === DECREASE_SPEED && speedIdx > 0) {
+            speedIdx -= 1
+        }
+        setPlayerSpeed(speedIdx)
+    })
+
+    const setPlayerSpeed = (speedIdx) => {
+        const videoPlayer = document.getElementsByTagName('video')[0];
+        videoPlayer.playbackRate = speedMultiplyers[speedIdx]
+        console.log(`Now speeding at ${speedMultiplyers[speedIdx]}`)
     }
-    const videoPlayer = document.getElementsByTagName('video')[0];
-    videoPlayer.playbackRate = speedMultiplyers[speedIdx]
-});
+
+    setPlayerSpeed(speedIdx)
+})()
