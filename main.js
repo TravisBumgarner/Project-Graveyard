9b8a190cb8e240200af39396b5346abf009994a7
Travis Bumgarner <travis.bumgarner@gmail.com>
2019-01-04 19:07:21 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 (()=> {
     let speedIdx = 3 // For me, defaults to 2x speed.
-    const speedMultiplyers = [0.5, 0.75, 1, 2, 4, 6, 8]
+    const speedMultiplyers = [0.5, 0.75, 1, 2, 2.5, 3, 3.5, 4, 4.5, 5, 6, 7, 8, 9, 10, 15, 20, 25, 30]
 
     // Visit https://keycode.info/ to remap the keys below.
     const DECREASE_SPEED = 219 // this is the ] char
