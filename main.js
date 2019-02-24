80fb7ffaca69d8374c3bea69675d86bcf5f90a96
Travis Bumgarner <travis.bumgarner@gmail.com>
2019-02-24 15:11:53 -0500

    Rewrote script

--- main.js
+++ main.js
@@ -1,71 +1,45 @@
 // User Settings
-const DEFAULT_SPEED_INDEX = 3
-const SPEED_MULTIPLYERS = [0.5, 0.75, 1, 2, 2.5, 3, 3.5, 4, 4.5, 5, 6, 7, 8, 9, 10, 15] // Max multiplyer is 15x.
-const DECREASE_SPEED = 219 // this is the [ char by default. Visit https://keycode.info/ to remap the keys below.
-const INCREASE_SPEED = 221 // this is the ] char by default.
+const DECREASE_SPEED = 219; // this is the [ char by default. Visit https://keycode.info/ to remap the keys below.
+const INCREASE_SPEED = 221; // this is the ] char by default.
+const INITIATE_PLAYER = 220; // this is the \ char by default.
 // End User Settings
 
-let speedIdx = DEFAULT_SPEED_INDEX
-let currentVideoPlayerId = null
+let currentSpeed;
+let videoPlayer;
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
+const updateSpeedDisplay = value => {
+  document.getElementById("videoPlayerDisplay").innerHTML = `Speed: ${
+    videoPlayer.playbackRate
+  }`;
+};
 
-const setPlayerSpeed = (speedIdx) => {
-    const videoPlayer = document.getElementsByTagName('video')[0];
-
-    if(!videoPlayer){return}
-
-    videoPlayer.playbackRate = SPEED_MULTIPLYERS[speedIdx]
-    console.log(`Now speeding at ${SPEED_MULTIPLYERS[speedIdx]}`)
-}
-
-document.addEventListener('keydown', event => {
-    if(event.keyCode === INCREASE_SPEED && speedIdx < SPEED_MULTIPLYERS.length){
-        speedIdx += 1
-    } else if (event.keyCode === DECREASE_SPEED && speedIdx > 0) {
-        speedIdx -= 1
+document.addEventListener("keydown", event => {
+  if (event.keyCode === INITIATE_PLAYER) {
+    videoPlayer = document.getElementsByTagName("video")[0];
+    if (!videoPlayer) {
+      console.log("no video found");
+      return;
+    } else {
+      var newDiv = document.createElement("div");
+      var newContent = document.createTextNode(
+        `Speed: ${videoPlayer.playbackRate}`
+      );
+      newDiv.prepend(newContent);
+      newDiv.id = "videoPlayerDisplay";
+      newDiv.style =
+        "position: fixed; top: 0px !important; left: 0px !important; display: inline-block; z-index: 99999; color: white; background-color: black; opacity: 0.25;";
+      document.body.appendChild(newDiv);
     }
-    setPlayerSpeed(speedIdx)
-})
+  }
 
-const addVideoEventListeners = (videoPlayer) => {
-    // When player pauses and resumes on some sites, the speed resets itself.
-    videoPlayer.addEventListener('play', () => {
-        console.log(`Now resuming at ${SPEED_MULTIPLYERS[speedIdx]}`)
-        setPlayerSpeed(speedIdx)
-    })
+  if (event.keyCode === INCREASE_SPEED || event.keyCode === DECREASE_SPEED) {
+    const currentPlaybackRate = videoPlayer.playbackRate;
+    const multiplier = currentPlaybackRate >= 4 ? 2 : 0.5;
+    const direction = event.keyCode === INCREASE_SPEED ? 1 : -1;
 
-    // When current video is done, get ready to apply settings to next video.
-    videoPlayer.addEventListener('ended', () => {
-        applySettingsToNewVideo()
-    })
-}
+    const newPlaybackRate = currentPlaybackRate + direction * multiplier;
 
-const applySettingsToNewVideo = async () => {
-    let nextVideoPlayerId
-    let nextVideoPlayer
-
-    while(currentVideoPlayerId === nextVideoPlayerId || typeof nextVideoPlayerId === 'undefined'){
-        await sleep(50)
-        nextVideoPlayer = document.getElementsByTagName('video')[0]
-
-        if (typeof nextVideoPlayer === "undefined"){continue}
-        nextVideoPlayerId = nextVideoPlayer.id
-    }
-    currentVideoPlayerId = nextVideoPlayerId
-    addVideoEventListeners(nextVideoPlayer)
-    setPlayerSpeed(speedIdx)
-}
-
-window.addEventListener('load', () => {
-    const videoPlayer = document.getElementsByTagName('video')[0];
-
-    if(!videoPlayer){return}
-
-    currentVideoPlayerId = videoPlayer.id
-
-    setPlayerSpeed(speedIdx)
-    addVideoEventListeners(videoPlayer)
-}, false);
+    videoPlayer.playbackRate = newPlaybackRate;
+    updateSpeedDisplay(newPlaybackRate);
+  }
+});
