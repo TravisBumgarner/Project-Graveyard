7a19f9e4374ae802d6b2215dba9c3b8b97d061f2
Travis Bumgarner <travis.bumgarner@gmail.com>
2019-04-27 12:27:22 -0400

    Can animate now

--- src/App/index.js
+++ src/App/index.js
@@ -1,3 +1,5 @@
+// TODO: Framerate will fail at 0
+
 import React, { useState } from 'react'
 
 const makeFrameState = (key, content = '') => ({
@@ -8,8 +10,11 @@ const makeFrameState = (key, content = '') => ({
 const App = () => {
     const [frameHeight, setFrameHeight] = useState(10)
     const [frameWidth, setFrameWidth] = useState(30)
-    const [frames, setFrames] = useState([])
+    const [frames, setFrames] = useState([{ key: -1, content: 'hi' }])
     const [nextFrameKey, setNextFrameKey] = useState(1)
+    const [frameRate, setFrameRate] = useState(30)
+    const [visibleFrameIndex, setVisibleFrameIndex] = useState(0)
+    const [isAnimating, setIsAnimating] = useState(false)
 
     const createFrame = () => {
         setFrames([...frames, makeFrameState(nextFrameKey)])
@@ -39,17 +44,47 @@ const App = () => {
             {content}
         </textarea>
     ))
+
+    const animate = () => {
+        let counter = 0
+        setIsAnimating(true)
+        setInterval(() => {
+            counter += 1
+            setVisibleFrameIndex(counter)
+        }, 1 / frameRate)
+    }
+
     return (
-        <div>
+        <>
             <div>
-                Add Frame: <button onClick={() => createFrame()}>+</button>
-                Set Width: <button onClick={() => setFrameWidth(frameWidth + 1)}>+</button>
-                <button onClick={() => setFrameWidth(frameWidth - 1)}>-</button>
-                Set Height: <button onClick={() => setFrameHeight(frameHeight + 1)}>+</button>
-                <button onClick={() => setFrameHeight(frameHeight - 1)}>-</button>
+                <div>
+                    Add Frame: <button onClick={() => createFrame()}>+</button>
+                    Set Width: <button onClick={() => setFrameWidth(frameWidth + 1)}>+</button>
+                    <button onClick={() => setFrameWidth(frameWidth - 1)}>-</button>
+                    Set Height: <button onClick={() => setFrameHeight(frameHeight + 1)}>+</button>
+                    <button onClick={() => setFrameHeight(frameHeight - 1)}>-</button>
+                </div>
+                {Frames}
             </div>
-            {Frames}
-        </div>
+            <div>
+                <div>
+                    Frame Rate: {frameRate}
+                    Set Frame Rate: <button onClick={() => setFrameRate(frameRate + 1)}>+</button>
+                    <button onClick={() => setFrameRate(frameRate - 1)}>-</button>
+                    {frames.length ? <button onClick={animate}>Animate</button> : null}
+                </div>
+                {isAnimating && frames.length ? (
+                    <div>
+                        {visibleFrameIndex}
+                        <textarea
+                            value={frames[visibleFrameIndex % frames.length].content}
+                            rows={frameHeight}
+                            cols={frameWidth}
+                        />
+                    </div>
+                ) : null}
+            </div>
+        </>
     )
 }
 
