cb199e8e7acc53474af368a8ec235cb8724ab421
Travis Bumgarner <travis.bumgarner@gmail.com>
2019-04-27 11:11:52 -0400

    Can do multiple textboxes now

--- src/App/index.js
+++ src/App/index.js
@@ -1,19 +1,27 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 
 const App = () => {
-  const [rows, setRows] = useState(10);
-  const [cols, setCols] = useState(30);
-  return (
-    <div>
-      <div>
-        Width <button onClick={() => setCols(cols + 1)}>++</button>
-        <button onClick={() => setCols(cols - 1)}>--</button>
-        Height <button onClick={() => setRows(rows + 1)}>++</button>
-        <button onClick={() => setRows(rows - 1)}>--</button>
-      </div>
-      <textarea rows={rows} cols={cols} />
-    </div>
-  );
-};
+    const [rows, setRows] = useState(10)
+    const [cols, setCols] = useState(30)
+    const [textAreas, setTextAreas] = useState(10)
 
-export default App;
+    const TextAreas = []
+    for (let i = 0; i < textAreas; i++) {
+        TextAreas.push(<textarea key={i} rows={rows} cols={cols} />)
+    }
+    return (
+        <div>
+            <div>
+                Text Boxes: <button onClick={() => setTextAreas(textAreas + 1)}>++</button>
+                <button onClick={() => setTextAreas(textAreas - 1)}>--</button>
+                Width <button onClick={() => setCols(cols + 1)}>++</button>
+                <button onClick={() => setCols(cols - 1)}>--</button>
+                Height <button onClick={() => setRows(rows + 1)}>++</button>
+                <button onClick={() => setRows(rows - 1)}>--</button>
+            </div>
+            {TextAreas}
+        </div>
+    )
+}
+
+export default App
