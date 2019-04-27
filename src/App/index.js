0f1980c6a85ebd0caf74a83e13769b329292d9ca
Travis Bumgarner <travis.bumgarner@gmail.com>
2019-04-27 11:36:34 -0400

    Can dynamically create textareas

--- src/App/index.js
+++ src/App/index.js
@@ -1,19 +1,49 @@
 import React, { useState } from 'react'
 
+const makeTextAreaState = (key, content = '') => ({
+    key,
+    content
+})
+
 const App = () => {
     const [rows, setRows] = useState(10)
     const [cols, setCols] = useState(30)
-    const [textAreas, setTextAreas] = useState(10)
+    const [textAreas, setTextAreas] = useState([])
+    const [nextTextAreaKey, setNextTextAreaKey] = useState(1)
 
-    const TextAreas = []
-    for (let i = 0; i < textAreas; i++) {
-        TextAreas.push(<textarea key={i} rows={rows} cols={cols} />)
+    const createTextArea = () => {
+        setTextAreas([...textAreas, makeTextAreaState(nextTextAreaKey)])
+        setNextTextAreaKey(nextTextAreaKey + 1)
     }
+
+    const updateTextAreaContent = (key, content) => {
+        const modifiedTextAreaState = textAreas.map(textArea => {
+            if (textArea.key === key) {
+                return makeTextAreaState(key, content)
+            } else {
+                return textArea
+            }
+        })
+        setTextAreas(modifiedTextAreaState)
+    }
+
+    const TextAreas = textAreas.map(({ content, key }) => (
+        <textarea
+            onChange={event => {
+                updateTextAreaContent(key, event.target.value)
+            }}
+            key
+            rows
+            cols
+        >
+            {content}
+        </textarea>
+    ))
+    console.log(textAreas)
     return (
         <div>
             <div>
-                Text Boxes: <button onClick={() => setTextAreas(textAreas + 1)}>++</button>
-                <button onClick={() => setTextAreas(textAreas - 1)}>--</button>
+                Text Boxes: <button onClick={() => createTextArea()}>++</button>
                 Width <button onClick={() => setCols(cols + 1)}>++</button>
                 <button onClick={() => setCols(cols - 1)}>--</button>
                 Height <button onClick={() => setRows(rows + 1)}>++</button>
