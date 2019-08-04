619af74cc216fa38d252f61578bfa6f5b137067c
Travis Bumgarner <travis.bumgarner@gmail.com>
2019-08-04 15:54:38 -0400

    commit prior to reformat

---
+++ sketch_jul20a/sketch_jul20a.ino
@@ -0,0 +1,16 @@
+#if(3 > 4)
+  #error that bad.
+#else
+  Serial.begin(9600);
+#endif
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
