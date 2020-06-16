04fffdd176877378b8006b35e2c7d7c7949dc03f
Travis Bumgarner <travis.bumgarner@gmail.com>
2020-06-15 21:53:46 -0400

    Added tons of stuff lol

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -9,6 +9,8 @@ import {
     FaYoutube as YouTube
 } from 'react-icons/fa'
 
+import { Calendar } from './components'
+
 import CAD_IMG from './media/cad.png'
 import CODE_IMG from './media/code.jpg'
 import CIRCUITS_IMG from './media/circuits.jpg'
@@ -19,27 +21,26 @@ const AppWrapper = styled.div`
     max-width: 1200px;
     margin: 15px auto 30px;
     box-sizing: border-box;
-    text-align: center;
     box-sizing: border-box;
+    padding: 0 1em 0;
 `
 
 const H1 = styled.h1`
     font-size: 3em;
-    padding: 1em;
+    margin: 1em 0 2em;
     font-weight: 700;
 `
 
 const H2 = styled.h2`
     font-size: 2.2em;
-    margin-bottom: 1em;
+    margin: 0.5em 0 0.5em 0;
     font-weight: 700;
 `
 
 const H3 = styled.h3`
     font-size: 1.4em;
-    margin-bottom: 0.5em;
     font-weight: 900;
-    margin-top: 1.5em;
+    margin: 0.5em 0 0.5em 0;
 `
 
 const Text = styled.p`
@@ -49,21 +50,36 @@ const Text = styled.p`
 `
 
 const Section = styled.div`
-    text-align: left;
     flex-basis: 0;
     flex-grow: 1;
-    margin: 5em 0;
     margin: 1em;
+
+    &:first-child {
+        margin-left: 0;
+    }
+
+    &:last-child {
+        margin-right: 0;
+    }
+
+    ${media.tablet} {
+        margin-left: 0;
+        margin-right: 0;    
+    }
 `
 
 const SectionWrapper = styled.div`
-    margin: 2em;
+    margin: 4em 0 6em 0;
+`
+
+const SectionContent = styled.div`
+
     display: flex;
     max-width: 100%;
     justify-content: space-between;
 
     ${media.tablet} {
-        flex-direction: column;
+        ${(({ nowrap }: { nowrap?: boolean }) => nowrap ? '' : 'flex-direction: column;')}
     }
 `
 
@@ -87,12 +103,9 @@ const CallToAction = styled.div`
 `
 
 const Img = styled.img`
-    width: 100%;
+    width: calc(90% / 3);
     height: auto;
     border-radius: 1em;
-
-    ${media.tablet} {
-    }
 `
 
 const AuthorImg = styled.img`
@@ -106,26 +119,6 @@ const AuthorImg = styled.img`
     ${media.tablet} {
         max-width: 150px;
         margin-right: 1em;
-    }
-`
-
-const ImageAndTextWrapper = styled.div`
-    width: 30%;
-    & > ${H3} {
-        display: none;
-    }
-    ${media.tablet} {
-        width: 100%;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-
-        & > ${H3} {
-            display: initial;
-        }
-        & > * {
-            width: 50%;
-        }
     }
 `
 
@@ -177,58 +170,88 @@ const App = () => {
             <AppWrapper>
                 <H1>Want to learn about 3D CAD, Code, and Circuits for free? No experience required!</H1>
                 <SectionWrapper>
-                    <ImageAndTextWrapper><Img src={CAD_IMG} /><H3>CAD!</H3></ImageAndTextWrapper>
-                    <ImageAndTextWrapper><H3>Code!</H3><Img src={CODE_IMG} /></ImageAndTextWrapper>
-                    <ImageAndTextWrapper><Img src={CIRCUITS_IMG} /><H3>Circuits!</H3></ImageAndTextWrapper>
+                    <SectionContent nowrap>
+                        <Img src={CAD_IMG} />
+                        <Img src={CODE_IMG} />
+                        <Img src={CIRCUITS_IMG} />
+                    </SectionContent>
                 </SectionWrapper>
                 <SectionWrapper>
-                    <Section>
-                        <H2>About the Author</H2>
-                        <div style={{ display: 'flex' }}>
-                            <AuthorImg src={AUTHOR_IMG} />
-                            <div>
-                                <Text>Travis Bumgarner has been a maker for almost 10 years. He studied mechanical engineering in university, launched an electrical engineering startup, and now works as a software engineer. In his free time he enjoys 3D modeling and printing, creating circuits, and programming websites and micro-controllers(mini computers).</Text>
-                                <ExternalLinksWrapper>
-                                    {ExternalLinks}
-                                </ExternalLinksWrapper>
+                    <H2>What You'll Learn</H2>
+                    <SectionContent>
+                        <Section>
+                            <H3>CAD!</H3>
+                            <List>
+                                <ListItem>Creating Parts</ListItem>
+                                <ListItem>Using External Libraries</ListItem>
+                                <ListItem>And much much more!</ListItem>
+                            </List>
+                        </Section>
+                        <Section>
+                            <H3>Code!</H3>
+                            <List>
+                                <ListItem>Arduino Tools</ListItem>
+                                <ListItem>Programming Basics wit C++</ListItem>
+                                <ListItem>And much much more!</ListItem>
+                            </List>
+                        </Section>
+                        <Section>
+                            <H3>Circuits!</H3>
+                            <List>
+                                <ListItem>Safety</ListItem>
+                                <ListItem>Prototyping with Breadboards</ListItem>
+                                <ListItem>PCB Design</ListItem>
+                                <ListItem>And much much more!</ListItem>
+                            </List>
+                        </Section>
+                    </SectionContent>
+                </SectionWrapper>
+                <SectionWrapper>
+                    <SectionContent>
+                        <Section>
+                            <H2>Schedule</H2>
+                            <Text>Workshops will be streamed live on <a target="_blank" href="https://www.twitch.tv/travis_the_maker">Twitch</a>. They'll be recorded and shared later on <a target="_blank" href="https://www.youtube.com/channel/UCFgIg95KzVg97KAeXdWbeXg">YouTube</a></Text>
+                            <Text>Click on Event Names for More Details. (Schedule subject to change.)</Text>
+                            <Calendar />
+                        </Section>
+                    </SectionContent>
+                </SectionWrapper>
+                <SectionWrapper>
+                    <SectionContent>
+                        <Section>
+                            <H2>Frequently Asked Questions</H2>
+
+                            <H3>How do I register?</H3>
+                            <Text><a target="_blank" href="https://forms.gle/HuUfqtbepWBAKq1m7">Click here!</a></Text>
+
+                            <H3>Will this be recorded?</H3>
+                            <Text>Yes! Recordings will be posted on Travis the Maker's <a target="_blank" href="https://www.youtube.com/channel/UCFgIg95KzVg97KAeXdWbeXg">YouTube Channel</a>. Now would be a great time to subscribe to the channel so you don't miss out!</Text>
+
+                            <H3>What software will we use and do I have to pay for it?</H3>
+                            <Text>All software is free to download. We'll be using the following:</Text>
+                            <List>
+                                <ListItem>Fusion 360 for 3D CAD - <a target="_blank" href="https://www.autodesk.com/campaigns/fusion-360-for-hobbyists">Download</a></ListItem>
+                                <ListItem>Eagle CAD for Circuits - <a target="_blank" href="https://www.autodesk.com/products/eagle/free-download">Download</a></ListItem>
+                                <ListItem>Arduino for Code - <a target="_blank" href="https://www.arduino.cc/en/Main/Software">Download</a></ListItem>
+                            </List>
+                        </Section>
+                    </SectionContent>
+                </SectionWrapper>
+                <SectionWrapper>
+                    <SectionContent>
+                        <Section>
+                            <H2>About the Author</H2>
+                            <div style={{ display: 'flex' }}>
+                                <AuthorImg src={AUTHOR_IMG} />
+                                <div>
+                                    <Text>Travis Bumgarner has been a maker for almost 10 years. He studied mechanical engineering in university, launched an electrical engineering startup, and now works as a software engineer. In his free time he enjoys 3D modeling and printing, creating circuits, and programming websites and micro-controllers(mini computers).</Text>
+                                    <ExternalLinksWrapper>
+                                        {ExternalLinks}
+                                    </ExternalLinksWrapper>
+                                </div>
                             </div>
-                        </div>
-                    </Section>
-                </SectionWrapper>
-                <SectionWrapper>
-                    <Section>
-                        <H2>When?</H2>
-                        <Text>July 6th - July 10th</Text>
-                        <Text>First Session: 10am - 12pm EST</Text>
-                        <Text>Second Session: 2pm - 4pm EST</Text>
-                    </Section>
-                    <Section>
-                        <H2>What?</H2>
-                        <Text>A series of classes related to CAD, code, and circuits. Register below for more details!</Text>
-                    </Section>
-                    <Section>
-                        <H2>Where?</H2>
-                        <Text>It will be streamed live on <a target="_blank" href="https://www.twitch.tv/travis_the_maker">Twitch</a>.</Text>
-                    </Section>
-                </SectionWrapper>
-                <SectionWrapper>
-                    <Section>
-                        <H2>Frequently Asked Questions</H2>
-
-                        <H3>How do I register?</H3>
-                        <Text><a target="_blank" href="https://forms.gle/HuUfqtbepWBAKq1m7">Click here!</a></Text>
-
-                        <H3>Will this be recorded?</H3>
-                        <Text>Yes! Recordings will be posted on Travis the Maker's <a target="_blank" href="https://www.youtube.com/channel/UCFgIg95KzVg97KAeXdWbeXg">YouTube Channel</a>. Now would be a great time to subscribe to the channel so you don't miss out!</Text>
-
-                        <H3>What software will we use and do I have to pay for it?</H3>
-                        <Text>All software is free to download. We'll be using the following:</Text>
-                        <List>
-                            <ListItem>Fusion 360 for 3D CAD - <a target="_blank" href="https://www.autodesk.com/campaigns/fusion-360-for-hobbyists">Download</a></ListItem>
-                            <ListItem>Eagle CAD for Circuits - <a target="_blank" href="https://www.autodesk.com/products/eagle/free-download">Download</a></ListItem>
-                            <ListItem>Arduino for Code - <a target="_blank" href="https://www.arduino.cc/en/Main/Software">Download</a></ListItem>
-                        </List>
-                    </Section>
+                        </Section>
+                    </SectionContent>
                 </SectionWrapper>
             </AppWrapper>
             <CallToAction>
