e6b9b43e5b3aa8a8f611607cb6f747be86b58fa8
Travis Bumgarner <travis.bumgarner@gmail.com>
2020-10-01 11:45:44 -0400

    I swear I'll get this decorator right...

--- python/audio_decorator.py
+++ python/audio_decorator.py
@@ -1,8 +1,9 @@
 def mario_or_bust(fcn):
     def wrappers(*args, **kwargs):
         try:
-            return fcn(*args, **kwargs)
+            results = fcn(*args, **kwargs)
             os.system('afplay /Users/travis-bumgarner/Programming/smb_stage_clear.wav -v 0.05')
+            return results
 
         except Error as e:
             print(e)
