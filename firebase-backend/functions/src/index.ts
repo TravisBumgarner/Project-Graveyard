bf1982bbe293e9f6f1a21f0cd49f43e703bc6395
Travis Bumgarner <travis.bumgarner@gmail.com>
2022-02-20 22:04:03 -0700

    sighhh

--- firebase-backend/functions/src/index.ts
+++ firebase-backend/functions/src/index.ts
@@ -1,11 +1,44 @@
-import * as functions from "firebase-functions";
 import * as express from 'express'
 import * as cors from 'cors'
+import { ConnectionOptions, createConnection } from "typeorm";
+
+const ENTITIES_DIR = __dirname + '/entity'
+const MIGRATIONS_DIR = __dirname + '/migration'
+
+const ormconfig = {
+    host: '',
+    port: 5432,
+    username: '',
+    password: '',
+    database: 'postgres',
+    type: 'postgres',
+    synchronize: false,
+    logging: false,
+    entities: [ENTITIES_DIR + '/**/*{.ts,.js}'],
+    migrations: [MIGRATIONS_DIR + '/**/*{.ts,.js}'],
+    cli: {
+        entitiesDir: ENTITIES_DIR,
+        migrationsDir: MIGRATIONS_DIR,
+    },
+} as ConnectionOptions
+
+
 
 const app = express();
 
 app.use(cors({ origin: true }));
 
-app.get('/ping', (req: express.Request, res: express.Response) => res.send('Hi'));
+app.get('/ping', (req: express.Request, res: express.Response) => res.send());
 
-exports.api = functions.https.onRequest(app);
+app.get('/db', async (req: express.Request, res: express.Response) => {
+    const connection = await createConnection(ormconfig)
+    const name = connection.name
+    return res.send(name)
+})
+
+app.listen(5001, () => console.log('running'))
+
+if (!(require.main === module)) {
+    const functions = require("firebase-functions");
+    exports.api = functions.https.onRequest(app);
+}
