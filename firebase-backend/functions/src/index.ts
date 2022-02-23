a7a3a9cbd5f8be24c98be54286f2858c41f4b40c
Travis Bumgarner <travis.bumgarner@gmail.com>
2022-02-22 19:15:31 -0700

    I need fresh

--- firebase-backend/functions/src/index.ts
+++
@@ -1,8 +0,0 @@
-import app from './app'
-
-if (process.env.NODE_ENV === 'development') {
-    app.listen(5001, () => console.log('running'))
-} else {
-    const functions = require("firebase-functions");
-    exports.api = functions.https.onRequest(app);
-}
