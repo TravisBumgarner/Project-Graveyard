1da6db55f7db694e0b24f6d85b6d4f7ca2d14347
Travis Bumgarner <travis.bumgarner@gmail.com>
2022-03-01 20:42:04 -0700

    Added sentry on frontend

--- frontend/src/App/components/Error.tsx
+++ frontend/src/App/components/Error.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import { H2, Paragraph } from './StyleExploration'
+import { H2, Paragraph, Link } from './StyleExploration'
 
 const Error = () => {
     return (
         <div>
             <H2>Whoops!</H2>
             <Paragraph>Sorry, there was an error, please try again later.</Paragraph>
+            <Paragraph> <Link href="https://forms.gle/H16iith5PhytP5D9A">Please let us know what you were doing so we can improve!</Link></Paragraph>
+
         </div>
     )
 }
