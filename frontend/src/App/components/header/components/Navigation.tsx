8b692ff8184a5a4f36a02439321bd2f210bebba7
Travis Bumgarner <travis.bumgarner@gmail.com>
2022-03-12 14:58:54 -0700

    Fixed bug with Navigation URLs

--- frontend/src/App/components/header/components/Navigation.tsx
+++ frontend/src/App/components/header/components/Navigation.tsx
@@ -22,30 +22,30 @@ const StyledNav = styled.ul`
     }
 `
 
+const ALWAYS_VISIBLE_LINKS = [
+    { text: 'Home', to: '/' },
+]
+
+const loggedInLinks = (userId: string) => ([
+    { text: 'Student Dashboard', to: '/student/dashboard' },
+    { text: 'Reviewer Dashboard', to: '/reviewer/dashboard' },
+    { text: 'Profile', to: `/profile/${state.currentUser.phraseADay.id}` },
+    { text: 'Users', to: '/users' },
+    { text: 'Log Out', to: '/logout' },
+])
+
+const LOGGED_OUT_VISIBLE_LINKS = [
+    { text: 'Log In', to: '/login' },
+    { text: 'Sign Up', to: '/signup' },
+]
+
 const Navigation = () => {
     const { state } = React.useContext(context)
     const [showNav, setShowNav] = React.useState<boolean>(false)
 
-    const ALWAYS_VISIBLE_LINKS = [
-        { text: 'Home', to: '/' },
-    ]
-
-    const LOGGED_IN_VISIBLE_LINKS = [
-        { text: 'Student Dashboard', to: '/student/dashboard' },
-        { text: 'Reviewer Dashboard', to: '/reviewer/dashboard' },
-        { text: 'Profile', to: `/profile/${state.currentUser.phraseADay.id}` },
-        { text: 'Users', to: '/users' },
-        { text: 'Log Out', to: '/logout' },
-    ]
-
-    const LOGGED_OUT_VISIBLE_LINKS = [
-        { text: 'Log In', to: '/login' },
-        { text: 'Sign Up', to: '/signup' },
-    ]
-
     const links = [
         ...ALWAYS_VISIBLE_LINKS,
-        ...(state.currentUser ? LOGGED_IN_VISIBLE_LINKS : LOGGED_OUT_VISIBLE_LINKS),
+        ...(state.currentUser ? loggedInLinks(state.currentUser.phraseADay.id) : LOGGED_OUT_VISIBLE_LINKS),
     ]
 
     const handleClose = () => setShowNav(false)
