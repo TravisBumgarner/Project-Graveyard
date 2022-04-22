eabf25182001b3f27b0edaeef97a27514127f039
Travis Bumgarner <travis.bumgarner@gmail.com>
2022-04-22 13:10:38 +0200

    Tweaking loading icon

--- frontend/src/sharedComponents/Loading.tsx
+++ frontend/src/sharedComponents/Loading.tsx
@@ -4,6 +4,9 @@ import React from 'react'
 import logo from '../static/logo.png'
 import { Heading } from '.'
 
+const ROTATION_TIME_INCREMENT = 15
+const TIME_TO_NOT_ROTATE = 50
+
 type LoadingProps = {
     fullscreen?: boolean
 }
@@ -33,15 +36,26 @@ const LoadingWrapper = styled.div`
 `
 
 const Loading = ({ fullscreen }: LoadingProps) => {
-    // const rotations = [0, 90, 180, 270]
     const [rotation, setRotation] = React.useState<number>(0)
+    const [stopRotationCounter, setStopRotationCounter] = React.useState<number>(0)
+
     React.useEffect(() => {
         const rotationIntervalId = setInterval(() => {
-            setRotation((prev) => prev + 1)
-        }, 25)
+            if (rotation % 180 === 0) {
+                if (stopRotationCounter === TIME_TO_NOT_ROTATE) {
+                    setStopRotationCounter(0)
+                    setRotation((prev) => prev + 1)
+                } else {
+                    setStopRotationCounter((prev) => prev + 1)
+                }
+            } else {
+                setRotation((prev) => prev + 1)
+            }
+        }, ROTATION_TIME_INCREMENT)
 
         return () => clearInterval(rotationIntervalId)
-    }, [])
+    }, [rotation, stopRotationCounter])
+
     return (
         <LoadingWrapper fullscreen={fullscreen}>
             <img style={{ transform: `rotate(${rotation}deg)` }} alt="logo" src={logo} />
