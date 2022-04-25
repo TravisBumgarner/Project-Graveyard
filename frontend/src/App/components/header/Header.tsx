e5eb6d9266d72b482632c538be0eb80374b6eaed
Travis Bumgarner <travis.bumgarner@gmail.com>
2022-04-25 12:46:54 +0200

    PR cleanup

--- frontend/src/App/components/header/Header.tsx
+++ frontend/src/App/components/header/Header.tsx
@@ -2,18 +2,9 @@ import React from 'react'
 import styled from 'styled-components'
 
 import { Link } from 'react-router-dom'
-import { colors, Heading, Paragraph } from 'sharedComponents'
+import { colors, Heading } from 'sharedComponents'
 import { context } from 'context'
 import { Navigation } from './components'
-// import logo from '../../../static/logo.png'
-
-// const Img = styled.img`
-//     width: 32px;
-//     height: 32px;
-//     margin-right: 1em;
-//     box-sizing: border-box;
-//     /* transform: rotate(-16deg); */
-// `
 
 const HeaderWrapper = styled.div`
     display:flex;
@@ -24,16 +15,12 @@ const HeaderWrapper = styled.div`
         display: flex;
         align-items: center;
     }
-
-
 `
 
 const Header = () => {
-    const { state } = React.useContext(context)
     return (
         <HeaderWrapper>
             <div>
-                {/* <Link to="/"> <Img src={logo} /></Link> */}
                 <Link style={{ textDecoration: 'none' }} to="/">
                     <Heading.H1>
                         <span style={{ color: colors.SECONDARY.base }}>phrase</span>
