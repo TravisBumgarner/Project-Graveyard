391b5ba1d238357b51dd4d8ec8f5d116c59bb615
Travis Bumgarner <travis.bumgarner@gmail.com>
2022-07-24 17:48:17 -0400

    not sure I solved anything but eyyy

--- frontend/webpack.js
+++ frontend/webpack.js
@@ -1,45 +1,95 @@
 const path = require('path')
+const webpack = require('webpack')
 
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-module.exports = {
+const PLUGIN_VARS = {
+  local: {
+    __API_ENDPOINT__: "'http://localhost:5001'",
+    __FIREBASE_CONFIG__: "'staging'",
+    __LOGGING_LEVEL__: "'local'"
+  },
+  staging: {
+    __API_ENDPOINT__: "'https://backend-rhtcuzbcma-uc.a.run.app'",
+    __FIREBASE_CONFIG__: "'staging'",
+    __LOGGING_LEVEL__: "'sentry'"
+  },
+  production: {
+    __API_ENDPOINT__: "' https://backend-sj2er5ckba-uc.a.run.app/'",
+    __FIREBASE_CONFIG__: "'production'",
+    __LOGGING_LEVEL__: "'sentry'"
+  }
+}
+
+const getEnvVariables = () => {
+  if (!process.env.NODE_ENV) {
+    throw new Error('NODE_ENV is not defined.')
+  }
+  return PLUGIN_VARS[process.env.NODE_ENV]
+}
+
+const envVariables = getEnvVariables()
+
+const webpackConfig = {
   entry: './src/index.tsx',
   output: {
     filename: 'app.bundle.js',
-    path: path.resolve(__dirname, 'public'),
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/',
+    pathinfo: false
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true,
+              experimentalWatchApi: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
       },
     ],
   },
+
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
     alias: {
       sharedComponents: path.resolve(__dirname, 'src/sharedComponents/'),
-      sharedModals: path.resolve(__dirname, 'src/sharedModals/'),
       sharedTypes: path.resolve(__dirname, 'src/sharedTypes.ts'),
       theme: path.resolve(__dirname, 'src/theme.tsx'),
       utilities: path.resolve(__dirname, 'src/utilities.ts'),
-      Context: path.resolve(__dirname, 'src/Context.tsx'),
+      types: path.resolve(__dirname, 'src/types.ts'),
+      context: path.resolve(__dirname, 'src/Context'),
     },
   },
-  devtool: 'source-map',
   devServer: {
     compress: true,
     port: 3000,
+    host: '0.0.0.0',
     hot: true,
     historyApiFallback: true,
   },
   plugins: [
+    new webpack.DefinePlugin(envVariables),
     new HtmlWebpackPlugin({
       template: './src/static/index.template.ejs',
       favicon: './src/static/favicon.png',
       inject: 'body',
     }),
   ],
+  devtool: 'inline-source-map'
 }
+
+module.exports = webpackConfig
