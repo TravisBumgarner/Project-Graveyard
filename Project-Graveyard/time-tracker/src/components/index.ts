95073c649a7d359b8ed494de873b1f03ce91ffa6
Travis Bumgarner <travis.bumgarner@gmail.com>
2025-01-26 02:06:06 -0600

    pure chaos

--- Project-Graveyard/time-tracker/src/components/index.ts
+++
@@ -1,9 +0,0 @@
-import Router from './Router'
-import Header from './Header'
-import Message from './Message'
-
-export {
-  Router,
-  Header,
-  Message
-}
--- Project-Graveyard/time-tracker/src/pages/index.ts
+++
@@ -1,6 +0,0 @@
-import Settings from '../modals/Settings'
-import Charts from './Charts'
-import Error from './Error'
-import TimeTracker from './TimeInput'
-
-export { Charts, Error, Settings, TimeTracker }
